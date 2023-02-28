339b56732ab5d196ab2a4128def146a944694a75
papa matar fall <61315626+PaM02@users.noreply.github.com>
2023-02-28 12:11:19 +0000

    update route inscription

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cours
+# dependence thymleaf devtool, spring web
+# 1-creer un fichier index.html dans le repertoire static de ce fait que si le erveur demarre cette page sera 
+# charger
+
+# 2-controller
+# pour dir qu'une classe est un controller il faut ajouter une annotation @controller
+
+# pour passer une variable du controle au vue on ajoute un params modelMap
+# ajouter ça dans le template <html lang="en" xmlns:th="http://www.thymeleaf.org">
+# en ajouter ça pour recuperer la valeur <h1 th:text="${name}"></h1>
  
---
+++ src/main/java/com/cours/cours/controllers/PagesController.java
@@ -0,0 +1,67 @@
+package com.cours.cours.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class PagesController {
+
+    /*
+     * le GetMapping c'est lorsqu'on fait une requette get
+     * cette methode reupere l'url ici /
+     * cette methode devait retourner normalement
+     * une vue du nom de Hello world un template
+     * mais on ajouter ReponseBody pour qu'elle retourne
+     * une reponse helloworld
+     * on peut tester ça sur postman
+     */
+    // @GetMapping("/")
+    // @ResponseBody
+    // public String home() {
+    // return "hello world";
+    // }
+
+    /*
+     * lorsqu'on entre url / on sera
+     * rediriger vers lage home.html
+     * qui se trouve dans le repertoire pages
+     */
+
+    /*
+     * on peut aussi mettre ça sur le premier parametre HttpServletRequest
+     * httpServletRequest
+     */
+
+    @GetMapping("/")
+    public String home(@RequestParam String name, ModelMap map) {
+        /*
+         * http://localhost:8080/?name=matar
+         * si on entre cette url
+         */
+        /*
+         * on affiche ici la valeur
+         */
+        // System.out.println(httpServletRequest.getParameter("name"));
+
+        /*
+         * http://localhost:8080/
+         * si on entre cette url
+         * la variable name sera null
+         */
+
+        String name = httpServletRequest.getParameter("name") != null
+                && !httpServletRequest.getParameter("name").isEmpty() ? httpServletRequest.getParameter("name")
+                        : "world";
+
+        map.put("name", name);
+
+        return "pages/home";
+
+    }
+
+}
